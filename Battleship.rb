ec7bb1b59f3967c1c1dbce644dcac9f24d7ef04e
Simon <simonturner34@gmail.com>
2014-12-06 21:28:42 +0000

    added method to get user input

--- Battleship.rb
+++ Battleship.rb
@@ -26,4 +26,17 @@ def render_grid(grid_array)
 
 end
 
+def get_input(user_input)
+
+	input_coords = user_input.split
+
+	return input_coords
+
+end
+
+puts "Choose your target coordinates (e.g. 2 3)"
+input = gets.chomp
+
+get_input(input)
+
 render_grid(grid_array)
